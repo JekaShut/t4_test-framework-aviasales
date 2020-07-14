c324aeb07e47b13d43ac14dbd007dc1ded1878bf
Eugene <e2.shut@qa-academy.by>
2020-07-14 19:46:07 +0300

    working board

--- index.py
+++ index.py
@@ -20,7 +20,7 @@ board3 = [  "-","-","-","-","-",
 class Game:
     def __init__(self, actualPlayer = "X"):
         self.actualPlayer = "X"
-    def __boards(self, key = None, actualPlayer = "X"):
+    def __boards(self, key = None, actualPlayer = "X", board = "board1"):
         if key == None:
             pass
         else:
@@ -46,25 +46,31 @@ class Game:
 
         return(board1) 
 
+    def selectBoard(self):
+        x = input("Выберите вариант игры: 1 - 3x3, 2 - 4x4, 3 - 5x5 : ")
+        board = "board" + x
+        Game().startGame(stop=0,board = board)
+
     def display_board(self):
         print(Game().__boards()[0] + "|" + Game().__boards()[1] + "|" + Game().__boards()[2]) 
         print(Game().__boards()[3] + "|" + Game().__boards()[4] + "|" + Game().__boards()[5])
         print(Game().__boards()[6] + "|" + Game().__boards()[7] + "|" + Game().__boards()[8])
     
-    def startGame(self, stop = 0):
+    def startGame(self, stop = 0, board = "board1"):
         while stop == 0:
+            print(board)
             Game().randomSet()
             Game().check_winner()
-            Game().check_end()
-            Game().actualTurn(self.actualPlayer) 
+            Game().check_end(board = board)
+            Game().actualTurn(self.actualPlayer, board = board) 
             Game().check_winner() 
-            Game().check_end() 
+            Game().check_end(board = board) 
         else:
             Game().display_board()
             print("GAME OVER!") 
             sys.exit(0)            
 
-    def actualTurn(self, actualPlayer):
+    def actualTurn(self, actualPlayer, board = "board1"):
         Game().display_board()
         actualPlayer = "X"
         error = 1
@@ -77,7 +83,7 @@ class Game:
         key = key - 1
         
         if key in range(0,9):
-            Game().__boards(key)
+            Game().__boards(key, board = board)
         else: 
             print("not in range")
 
@@ -90,7 +96,7 @@ class Game:
             key = random.randint(0,8)
             Game().__boards(key, actualPlayer)
     
-    def check_end(self): 
+    def check_end(self, board = "board1"): 
         if board1.count("-") == 0:
             return(True)
         else: 
@@ -129,5 +135,4 @@ class Game:
         
         
     
-Game().startGame()  
-
+Game().selectBoard() 
