0e9136939480be554ee7446ad52843f565890dad
Eugene <e2.shut@qa-academy.by>
2020-07-14 13:18:01 +0300

    Turn change

--- index.py
+++ index.py
@@ -9,14 +9,26 @@ class Game:
         board1 = [  "-","-","-",
                     "-","-","-",
                     "-","-","-",    ]
-        return(board1)
+        return(board1) #добавить выбор доски
     def display_board(self):
         print(Game().__boards()[0] + "|" + Game().__boards()[1] + "|" + Game().__boards()[2])
         print(Game().__boards()[3] + "|" + Game().__boards()[4] + "|" + Game().__boards()[5])
         print(Game().__boards()[6] + "|" + Game().__boards()[7] + "|" + Game().__boards()[8])
     
+    def startGame(self):
+        while self.game_is_on:
+            Game().changeTurn(self.actualPlayer)
+
+    def changeTurn(self, actualPlayer):
+        key = input("enter pos from 1 to 9: ") #тут добавить isinstanse int
+        key = int(key) - 1
+        Game().__boards()[key] = "X"
+        Game().display_board()
+        
+    def changePlayer(self):
+        pass
 
 
         
-Game().display_board()    
+Game().startGame()  
 
