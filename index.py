6098238e30ab2bda00e942d1a40df07e37f86daa
Eugene <e2.shut@qa-academy.by>
2020-07-15 12:11:58 +0300

    Refactoring + add some comments

--- index.py
+++ index.py
@@ -19,36 +19,13 @@ board3 = [  "-","-","-","-","-",
 
             
 class Game:
-    def __init__(self, actualPlayer = "X"):
-        self.actualPlayer = "X"
-    def __boards(self, key = None, actualPlayer = "X", board = [board1, board2, board3], num = 0):
-        
-        if key == None:
-            pass
-        else:
-            if actualPlayer == "X":
-                
-                if board[num][key] == "0":
-                    print("Nope. It's alredy taken")
-                    Game().actualTurn("X", num=num)
-                if board[num][key] == "X":
-                    print("Nope. It's alredy taken")
-                    Game().actualTurn("X", num=num)
-                if board[num][key] == "-":
-                    board[num][key] = actualPlayer
+    #def __init__(self, actualPlayer = "X"):
+   #    #self.actualPlayer = "X"
+   #    pass
 
-            elif actualPlayer == "0":
-  
-                if board[num][key] == "X":
-                    Game().randomSet()
-                if board[num][key] == "0":
-                    Game().randomSet() 
-                if board[num][key] == "-":
-                    board[num][key] = actualPlayer
 
-        return(board[num]) 
-
-    def selectBoard(self):
+    def selectBoard(self): # Выбор доски для игры
+ 
         error = 1
         while error == True:
             try:
@@ -64,41 +41,45 @@ class Game:
             print("You must select number from 1 to 3")
             Game().selectBoard()
 
-    def display_board(self,num = 1):
-        
-        if num == 0:
-            
-            print(Game().__boards()[0] + "|" + Game().__boards()[1] + "|" + Game().__boards()[2] + "     1 | 2 | 3")
-            print(Game().__boards()[3] + "|" + Game().__boards()[4] + "|" + Game().__boards()[5] + "     4 | 5 | 6")
-            print(Game().__boards()[6] + "|" + Game().__boards()[7] + "|" + Game().__boards()[8] + "     7 | 8 | 9")
-        if num == 1:
-            print(Game().__boards(num=num)[0] + "|" + Game().__boards(num=num)[1] + "|" + Game().__boards(num=num)[2] + "|" + Game().__boards(num=num)[3] + "     1  | 2  | 3  | 4")
-            print(Game().__boards(num=num)[4] + "|" + Game().__boards(num=num)[5] + "|" + Game().__boards(num=num)[6] + "|" + Game().__boards(num=num)[7] + "     5  | 6  | 7  | 8")
-            print(Game().__boards(num=num)[8] + "|" + Game().__boards(num=num)[9] + "|" + Game().__boards(num=num)[10] + "|" + Game().__boards(num=num)[11] + "     9  | 10 | 11 | 12")
-            print(Game().__boards(num=num)[12] + "|" + Game().__boards(num=num)[13] + "|" + Game().__boards(num=num)[14] + "|" + Game().__boards(num=num)[15] + "     13 | 14 | 15 | 16")
-        if num == 2:
-            print(Game().__boards(num=num)[0] + "|" + Game().__boards(num=num)[1] + "|" + Game().__boards(num=num)[2] + "|" + Game().__boards(num=num)[3] + "|" + Game().__boards(num=num)[4] + "     1  | 2  | 3  | 4  | 5")
-            print(Game().__boards(num=num)[5] + "|" + Game().__boards(num=num)[6] + "|" + Game().__boards(num=num)[7] + "|" + Game().__boards(num=num)[8] + "|" + Game().__boards(num=num)[9] + "     6  | 7  | 8  | 9  | 10")
-            print(Game().__boards(num=num)[10] + "|" + Game().__boards(num=num)[11] + "|" + Game().__boards(num=num)[12] + "|" + Game().__boards(num=num)[13] + "|" + Game().__boards(num=num)[14] + "     11 | 12 | 13 | 14 | 15")
-            print(Game().__boards(num=num)[15] + "|" + Game().__boards(num=num)[16] + "|" + Game().__boards(num=num)[17] + "|" + Game().__boards(num=num)[18] + "|" + Game().__boards(num=num)[19] + "     16 | 17 | 18 | 19 | 20")
-            print(Game().__boards(num=num)[20] + "|" + Game().__boards(num=num)[21] + "|" + Game().__boards(num=num)[22] + "|" + Game().__boards(num=num)[23] + "|" + Game().__boards(num=num)[24] + "     21 | 22 | 23 | 24 | 25")
-
-    def startGame(self, stop = 0, num=0, board = [board1, board2, board3]):
+    
+    def startGame(self, stop = 0, num=0, board = [board1, board2, board3]): # старт игры, вызов основных функций , проверка на завершение игры
         num = num
         while stop == 0:
             
             Game().randomSet(num = num)
-            Game().check_winner(num = num)
+            Game().checkWin(num = num)
             Game().check_end(board = board, num = num)
-            Game().actualTurn(self.actualPlayer, board = board, num=num) 
-            Game().check_winner(num = num) 
+            Game().actualTurn(board = board, num=num, actualPlayer = "X")  #self.actualPlayer,
+            Game().checkWin(num = num) 
             Game().check_end(board = board, num = num) 
         else:
             Game().display_board(num = num)
             print("GAME OVER!") 
-            sys.exit(0)            
+            sys.exit(0)  
 
-    def actualTurn(self, actualPlayer, board = [board1, board2, board3], num = 0):
+    def __boards(self, key = None, actualPlayer = "X", board = [board1, board2, board3], num = 0): # Проверка не занято ли место под крестик либо нолик
+        
+        if key == None:
+            pass
+        else:
+            if actualPlayer == "X":
+                
+                if board[num][key] == "0" or board[num][key] == "X":
+                    print("Nope. It's alredy taken")
+                    Game().actualTurn("X", num=num)
+                if board[num][key] == "-":
+                    board[num][key] = actualPlayer
+
+            elif actualPlayer == "0":
+  
+                if board[num][key] == "X" or board[num][key] == "0":
+                    Game().randomSet()
+                if board[num][key] == "-":
+                    board[num][key] = actualPlayer
+
+        return(board[num])           
+
+    def actualTurn(self, actualPlayer, board = [board1, board2, board3], num = 0):  # Мой ход
         
         Game().display_board(num = num)
         actualPlayer = "X"
@@ -127,7 +108,7 @@ class Game:
                 print("not in range")
 
 
-    def randomSet(self, num=0):
+    def randomSet(self, num=0): # "Исскуственный интелект"
         if Game().check_end(num = num) == True:
             pass
         else:
@@ -135,18 +116,35 @@ class Game:
             key = random.randint(0,8)
             Game().__boards(key, actualPlayer, num=num)
     
-    def check_end(self, board = [board1, board2, board3], num = 0): 
+    def check_end(self, board = [board1, board2, board3], num = 0):   # проверка на то, закончились ли свободные клетки
         
         if board[num].count("-") == 0:
             return(True)
         else: 
             return(False)
-    
-    def check_winner(self, num = 0):
-        Game().checkWin(num = num)
+
+
+    def display_board(self,num = 1):  # отрисовка доски
+        
+        if num == 0:
+            
+            print(Game().__boards()[0] + "|" + Game().__boards()[1] + "|" + Game().__boards()[2] + "     1 | 2 | 3")
+            print(Game().__boards()[3] + "|" + Game().__boards()[4] + "|" + Game().__boards()[5] + "     4 | 5 | 6")
+            print(Game().__boards()[6] + "|" + Game().__boards()[7] + "|" + Game().__boards()[8] + "     7 | 8 | 9")
+        if num == 1:
+            print(Game().__boards(num=num)[0] + "|" + Game().__boards(num=num)[1] + "|" + Game().__boards(num=num)[2] + "|" + Game().__boards(num=num)[3] + "     1  | 2  | 3  | 4")
+            print(Game().__boards(num=num)[4] + "|" + Game().__boards(num=num)[5] + "|" + Game().__boards(num=num)[6] + "|" + Game().__boards(num=num)[7] + "     5  | 6  | 7  | 8")
+            print(Game().__boards(num=num)[8] + "|" + Game().__boards(num=num)[9] + "|" + Game().__boards(num=num)[10] + "|" + Game().__boards(num=num)[11] + "     9  | 10 | 11 | 12")
+            print(Game().__boards(num=num)[12] + "|" + Game().__boards(num=num)[13] + "|" + Game().__boards(num=num)[14] + "|" + Game().__boards(num=num)[15] + "     13 | 14 | 15 | 16")
+        if num == 2:
+            print(Game().__boards(num=num)[0] + "|" + Game().__boards(num=num)[1] + "|" + Game().__boards(num=num)[2] + "|" + Game().__boards(num=num)[3] + "|" + Game().__boards(num=num)[4] + "     1  | 2  | 3  | 4  | 5")
+            print(Game().__boards(num=num)[5] + "|" + Game().__boards(num=num)[6] + "|" + Game().__boards(num=num)[7] + "|" + Game().__boards(num=num)[8] + "|" + Game().__boards(num=num)[9] + "     6  | 7  | 8  | 9  | 10")
+            print(Game().__boards(num=num)[10] + "|" + Game().__boards(num=num)[11] + "|" + Game().__boards(num=num)[12] + "|" + Game().__boards(num=num)[13] + "|" + Game().__boards(num=num)[14] + "     11 | 12 | 13 | 14 | 15")
+            print(Game().__boards(num=num)[15] + "|" + Game().__boards(num=num)[16] + "|" + Game().__boards(num=num)[17] + "|" + Game().__boards(num=num)[18] + "|" + Game().__boards(num=num)[19] + "     16 | 17 | 18 | 19 | 20")
+            print(Game().__boards(num=num)[20] + "|" + Game().__boards(num=num)[21] + "|" + Game().__boards(num=num)[22] + "|" + Game().__boards(num=num)[23] + "|" + Game().__boards(num=num)[24] + "     21 | 22 | 23 | 24 | 25")
 
     
-    def checkWin(self, num = 0):
+    def checkWin(self, num = 0): # проверка победы
         if num == 0: #3x3
             row_1 = Game().__boards()[0] == Game().__boards()[1] == Game().__boards()[2] != "-"
             row_2 = Game().__boards()[3] == Game().__boards()[4] == Game().__boards()[5] != "-"
@@ -216,5 +214,4 @@ class Game:
             if diagonal_1 or diagonal_2:
                 Game().startGame(stop=1, num = num)
             
-
 Game().selectBoard() 
