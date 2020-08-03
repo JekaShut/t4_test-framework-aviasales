4f13b6f6c7ca5c82f649e1f58dea36cdecce7bbc
Eugene <e2.shut@qa-academy.by>
2020-08-03 16:37:42 +0300

    Waits are working fine [BaseElement.py]

--- framework/utils/BaseElement.py
+++ framework/utils/BaseElement.py
@@ -1,4 +1,7 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+
 from framework.Browser import *
 from abc import ABC
 
@@ -15,10 +18,14 @@ class BaseElement(ABC):
     def _find(self):
         wait = WebDriverWait(self.driver, WaitTime)
         if self.locatorType == "Xpath":
+            wait.until(EC.presence_of_element_located((By.XPATH, self.locator)))
             self.element = self.driver.find_element_by_xpath(self.locator)
         if self.locatorType == "Id":
+            wait.until(EC.presence_of_element_located((By.ID, self.locator)))
             self.element = self.driver.find_element_by_id(self.locator)
         if self.locatorType == "Class":
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, self.locator)))
             self.element = self.driver.find_element_by_class_name(self.locator)
         if self.locatorType == "Css":
+            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, self.locator)))
             self.element = self.driver.find_element_by_css_selector(self.locator)
