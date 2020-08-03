0ee6e3f59396cde883994f7718eba7f32da0c437
Eugene <e2.shut@qa-academy.by>
2020-08-03 20:40:23 +0300

    YouTrack test

--- pageObjects/pages/MainPage.py
+++ pageObjects/pages/MainPage.py
@@ -18,14 +18,14 @@ class MainPage:
         self.FromThereXpath = "//div[@class='trip-duration__input-wrapper --return']"
 
     def sendKeys(self):
-        time.sleep(3) #тут добавить wait
+        #time.sleep(3) #тут добавить wait
         ElementOperations.Input(locatorType="Xpath", locator=self.FromXpath).send(self.FromText)
         ElementOperations.Input(locatorType="Xpath", locator=self.ToXpath).send(self.ToText)
-        time.sleep(2)
+        #time.sleep(2)
         ElementOperations.Button(locatorType="Xpath", locator=self.ThereXpath).click()
-        ElementOperations.Input(locatorType="Xpath", locator=self.ValueToXpath).send(self.ValueTo) # тут сделать нажатие на кнопку
-        time.sleep(2)
+        # тут сделать нажатие на кнопку
+        #time.sleep(2)
         ElementOperations.Button(locatorType="Xpath", locator=self.FromThereXpath).click()
-        time.sleep(2)
+        time.sleep(4)
 
 
